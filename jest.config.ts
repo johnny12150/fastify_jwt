d6c667ca4c89df03d587a27c0701e0eee144bc14
wade <johnny12150@gmail.com>
2024-03-19 22:14:50 +0800

    Move code to src and type to @types

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    roots: ['<rootDir>'],
+    roots: ['<rootDir>/src'],
     testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest',
